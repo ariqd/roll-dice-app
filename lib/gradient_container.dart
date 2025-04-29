1d97adf9f0d1a7a22a827268a423786946552d24
ariqd <ariqdaffaathallah@gmail.com>
2025-04-29 20:43:26 +0700

    add styledtext props

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,14 +1,14 @@
 import 'package:dice_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var startAlignment = Alignment.topLeft;
-    var endAlignment = Alignment.bottomRight;
-
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
@@ -19,9 +19,9 @@ class GradientContainer extends StatelessWidget {
         backgroundColor: Colors.transparent,
       ),
       body: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           gradient: LinearGradient(
-            colors: const [
+            colors: [
               Color.fromARGB(255, 26, 2, 80),
               Color.fromARGB(255, 45, 7, 98),
             ],
@@ -30,7 +30,7 @@ class GradientContainer extends StatelessWidget {
           ),
         ),
         child: const Center(
-          child: StyledText(),
+          child: StyledText('Roll the Dice!'),
         ),
       ),
     );
--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-  const StyledText({super.key});
+  const StyledText(this.text, {super.key});
+
+  final String text;
 
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'Roll the dice!',
-      style: TextStyle(
+    return Text(
+      text,
+      style: const TextStyle(
         fontSize: 24,
         color: Colors.white,
-        fontWeight: FontWeight.bold,
       ),
     );
   }
